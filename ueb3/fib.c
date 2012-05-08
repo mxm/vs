dfab92943c6c85f5d2b146ce9e1d238871f82933
Sascha <p0mz0r@p0mz0r-VirtualBox.(none)>
2012-05-08 15:32:40 +0200

    aufgabe 2 vom 3. uebungsblatt

--- ueb3/fib.c
+++ ueb3/fib.c
@@ -5,29 +5,57 @@
 #include <omp.h>
 
 
-long long int fib(int n) {
-    if(n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        return fib(n-1) + fib(n-2);
-    }
+long long int fib(int n, int num_threads, int thread_id) {
+   	 long fib1, fib2;
+
+	int num_threads_left;
+	
+	if (n < 2) {
+		return n;
+	}
+	
+	#pragma omp
+	if (num_threads <=1) {
+		printf("[ID: %d][n: %d]\n", thread_id, n);
+		return (fib(n-1, 1, thread_id)+ fib(n-2, 1, thread_id));
+	}
+	
+	if ((num_threads % 2) == 1)
+	    num_threads_left = num_threads / 2 + 1;
+        else
+	    num_threads_left = num_threads / 2;
+
+	#pragma omp parallel sections num_threads(2)
+	{
+	  #pragma omp atomic
+		  thread_id++;
+	  
+	  #pragma omp section
+		  fib1 = fib(n-1, num_threads_left, thread_id);
+		  
+	  #pragma omp section
+		  fib2 = fib(n-2, num_threads-num_threads_left, thread_id+1);
+	}
+	return(fib1+fib2);
 }
 
 int main(int argc, char *argv[]) {
 
-    int n;
+    int n, num_threads;
+    long long int result;
 
 	/* Convert input to number */
 
     assert(argv[1] != NULL);
 
 	n = atoi(argv[1]);
+	num_threads = atoi(argv[2]);
 
     assert(n >= 0);
-
-    printf("fib(%d) = %lld\n", n, fib(n));
+    
+    result = fib(n, num_threads, 0);
+    
+    printf("fib(%d) = %lld\n", n, result);
 
     exit(0);
 
