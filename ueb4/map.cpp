1196cc6c0060b4ee34dfefd14a3bb3aa76841400
booo <borgers@mi.fu-berlin.de>
2012-05-13 13:05:50 +0200

    ueb4: implement deserialize function for map

--- ueb4/map.cpp
+++ ueb4/map.cpp
@@ -51,7 +51,7 @@ std::map<std::string, int> map(std::string filename) {
             std::stringstream linestream(line);
 
             //get words, split by whitespaces
-            while (getline(linestream, word, ' ')) {
+            while(getline(linestream, word, ' ')) {
                 ++wordcounts[word];
             }
         }
@@ -68,19 +68,52 @@ std::string mapserialize(std::map<std::string, int> map) {
 
     std::map<std::string, int>::iterator end = map.end();
     for(std::map<std::string, int>::iterator iter = map.begin(); iter != end; ++iter) {
-        result << iter->first << ":" << iter->second << std::endl;
+        result << iter->first << ":" << iter->second << " ";
     }
 
     return result.str();
 }
 
-std::map<std::string, int> map) {}
+//function to deserialize a map (string)
+std::map<std::string, int> mapdeserialize(std::string serializedmap) {
+
+    std::string tuple;
+    std::map<std::string, int> map;
+    std::stringstream input(serializedmap);
+
+    std::string word;
+    int count;
+    size_t position = 0;
+
+    //tokenize by whitespace
+    while(getline(input, tuple, ' ')) {
+
+        //find key-value delimiter
+        //reconstruct word and wordcount
+        position = tuple.find(":");
+        word = tuple.substr(0,position);
+        count = atoi(tuple.substr(position+1, tuple.length()).c_str());
+        map[word] = count; //feed data into result map
+    }
+
+    return map;
+}
+
+//debug function to print a map
+void printmap(std::map<std::string, int> map) {
+    std::map<std::string, int>::iterator end = map.end();
+    for(std::map<std::string, int>::iterator iter = map.begin(); iter != end; ++iter) {
+        std::cout << iter->first << ":" << iter->second << std::endl;
+    }
+}
 
 int main(int argc, char* argv[]) {
     //test of map function
-    std::cout << mapserialize(map("sample.txt"));
-
-
+    printmap(map("sample.txt"));
+    std::cout << "########################" << std::endl;
+    std::cout << mapserialize(map("sample.txt")) << std::endl;
+    std::cout << "########################" << std::endl;
+    printmap(mapdeserialize(mapserialize(map("sample.txt"))));
 
     return 0;
 }
