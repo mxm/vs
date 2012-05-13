b28558ac6d10cf566051a99bcb28b5088625b093
booo <borgers@mi.fu-berlin.de>
2012-05-13 16:09:55 +0200

    ueb4: implement a hashing algorithm

--- ueb4/main.cpp
+++ ueb4/main.cpp
@@ -6,6 +6,15 @@
 
 #include "map.hpp"
 
+//stolen from http://www.cse.yorku.ca/~oz/hash.html
+unsigned long hash(std::string s) {
+    unsigned long hash = 5381;
+
+    for(std::string::const_iterator iter = s.begin(); iter != s.end(); ++iter) {
+        hash = ((hash << 5) + hash) + *iter; /* hash * 33 + c */
+    }
+    return hash;
+}
 
 //merge a map into another map
 void mergemaps(std::map<std::string, int>& result, std::map<std::string, int> map) {
@@ -57,10 +66,40 @@ int main(int argc, char* argv[]) {
     printmap(localresult);
 #endif
 
+    std::vector<std::map<std::string, int> > remoteresults;
+
+    for(i = 0; i < numprocs; ++i) {
+        remoteresults.push_back(std::map<std::string, int>());
+    }
+
+    std::map<std::string, int>::iterator end2 = localresult.end();
+    for(std::map<std::string, int>::iterator iter = localresult.begin(); iter != end2; ++iter) {
+#ifdef DEBUG
+        std::cout << hash(iter->first) % numprocs << std::endl;
+#endif
+        int processid = (int) hash(iter->first) % numprocs;
+        remoteresults[processid][iter->first] = iter->second;
+    }
+
     //send local results that don't belong to process to n-1 other processes
     //what happens if number of files is smaller then process number?
     //do not communicate with all process?
 
+    for(i=0; i < numprocs; ++i) {
+#ifdef DEBUG
+        std::cout << mapserialize(remoteresults[i]) << std::endl;
+#endif
+        if(i == processid) {
+            //do not send data to yourself
+            //merge with reduce results ;)
+        } else {
+            //MPI_send to n-1 process
+            //send a string that represents the map for process x
+        }
+    }
+
+    //receive n-1 serialized maps from n-1 other process
+    //empty string equals no data?
 
 	MPI_Finalize();
 
--- ueb4/map.hpp
+++ ueb4/map.hpp
@@ -1,2 +1,5 @@
 std::map<std::string, int> map(std::string filename);
 void printmap(std::map<std::string, int>);
+
+std::map<std::string, int> mapdeserialize(std::string serializedmap);
+std::string mapserialize(std::map<std::string, int> map);
